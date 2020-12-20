1a5256b8ac9b810750b5f98462bbba35b01789bd
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-12-20 12:57:11 -0500

    remove connection to mongoose from seed.js

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,15 +1,4 @@
-let mongoose = require("mongoose");
 let db = require("../models");
-
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker",
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  }
-);
 
 let workoutSeed = [
   {
