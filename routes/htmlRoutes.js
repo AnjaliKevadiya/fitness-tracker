716e05dab6b8b179ecd2374a48fefc27436d722b
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-12-19 19:39:06 -0500

    defined endpoints for exercise.html and state.html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,4 +4,13 @@ const router = require("express").Router();
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/state", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
 module.exports = router;
