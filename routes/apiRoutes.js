b5550d83c726ff9fd5de2689fe101a30524100cf
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-12-20 14:26:38 -0500

    created api endpoint to create new workout to the database based on user's input

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-const db = require("../models/Workout");
+const Workout = require("../models/Workout.js");
 
+// api route to get all the workouts
 router.get("/api/workouts", (req, res) => {
-  db.find({}, (err, data) => {
+  Workout.find({}, (err, data) => {
     console.log(err);
     console.log(data);
     if (err) {
@@ -11,6 +12,24 @@ router.get("/api/workouts", (req, res) => {
       res.json(data);
     }
   });
+});
+
+// api route to create new workout record to database
+router.post("/api/workouts", ({ body }, res) => {
+  console.log("body", body);
+
+  const workout = new Workout(body);
+  console.log("workout", workout);
+
+  Workout.create(workout)
+    .then((workoutData) => {
+      console.log("workoutdata", workoutData);
+      res.json(workoutData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 module.exports = router;
